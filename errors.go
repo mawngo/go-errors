fb72891c6de758fc832c72d464e5fbb3e1138aba
mawngo <sitdownrightnow.dev@gmail.com>
2026-03-20 01:48:39 +0700

    feat: re-export AsType from standard errors package

--- errors.go
+++ errors.go
@@ -192,6 +192,23 @@ func Unwrap(err error) error {
 	return errors.Unwrap(err)
 }
 
+// AsType is a wrapper of built-in [errors.AsType]. It finds the first error in err's tree that matches the type E, and
+// if one is found, returns that error value and true. Otherwise, it
+// returns the zero value of E and false.
+//
+// The tree consists of err itself, followed by the errors obtained by
+// repeatedly calling its Unwrap() error or Unwrap() []error method. When
+// err wraps multiple errors, AsType examines err followed by a
+// depth-first traversal of its children.
+//
+// An error err matches the type E if the type assertion err.(E) holds,
+// or if the error has a method As(any) bool such that err.As(target)
+// returns true when target is a non-nil *E. In the latter case, the As
+// method is responsible for setting target.
+func AsType[E error](err error) (E, bool) {
+	return errors.AsType[E](err)
+}
+
 // Join is a wrapper of built-in [errors.Join]
 // Join returns an error that wraps the given errors.
 // Any nil error values are discarded.
